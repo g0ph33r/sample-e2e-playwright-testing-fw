0b0b7f4343c3d35da31554dd8b88868d043ad700
Piotr Pudysz <pmpudysz@gmail.com>
2024-01-22 17:33:14 +0100

    fix: faker special chars

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -9,14 +9,13 @@ test.describe('Verify register', () => {
     page,
   }) => {
     //Arrange
-    const userFirstName = faker.person.firstName();
-    const userLastName = faker.person.lastName();
+    const userFirstName = faker.person.firstName().replace(/[^A-Za-z]/g, '');
+    const userLastName = faker.person.lastName().replace(/[^A-Za-z]/g, '');
     const userEmail = faker.internet.email({
       firstName: userFirstName,
       lastName: userLastName,
     });
     const userPassword = faker.internet.password();
-
     const registerPage = new RegisterPage(page);
 
     //Act
