df3a7e25ccd91ff730636a10ffee2a82426ff842
Piotr Pudysz <pmpudysz@gmail.com>
2024-01-23 21:56:29 +0100

    chore: register user model

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,3 +1,4 @@
+import { RegisterUser } from '../src/models/user.model';
 import { LoginPage } from '../src/pages/login.page';
 import { RegisterPage } from '../src/pages/register.page';
 import { WelcomePage } from '../src/pages/welcome.page';
@@ -9,23 +10,22 @@ test.describe('Verify register', () => {
     page,
   }) => {
     //Arrange
-    const userFirstName = faker.person.firstName().replace(/[^A-Za-z]/g, '');
-    const userLastName = faker.person.lastName().replace(/[^A-Za-z]/g, '');
-    const userEmail = faker.internet.email({
-      firstName: userFirstName,
-      lastName: userLastName,
-    });
-    const userPassword = faker.internet.password();
     const registerPage = new RegisterPage(page);
 
+    const registerUserData: RegisterUser = {
+      userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
+      userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
+      userEmail: '',
+      userPassword: faker.internet.password(),
+    };
+
+    registerUserData.userEmail = faker.internet.email({
+      firstName: registerUserData.userFirstName,
+      lastName: registerUserData.userLastName,
+    });
     //Act
     await registerPage.goto();
-    await registerPage.register(
-      userFirstName,
-      userLastName,
-      userEmail,
-      userPassword,
-    );
+    await registerPage.register(registerUserData);
 
     const expectedAlertPopupText = 'User created';
 
@@ -36,7 +36,10 @@ test.describe('Verify register', () => {
     const title = await loginPage.title();
     expect(title).toContain('🦎 GAD | Login');
 
-    await loginPage.login(userEmail, userPassword);
+    await loginPage.login(
+      registerUserData.userEmail,
+      registerUserData.userPassword,
+    );
     const welcomePage = new WelcomePage(page);
     const titleWelcome = await welcomePage.title();
     expect(titleWelcome).toContain('🦎 GAD | Welcome');
