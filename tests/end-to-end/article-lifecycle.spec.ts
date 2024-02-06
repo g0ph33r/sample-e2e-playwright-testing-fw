108d331ab895f7ad7b540c9b4287896678c376b0
g0ph33r <pmpudysz@gmail.com>
2024-02-06 23:18:23 +0100

    remove test

--- tests/end-to-end/article-lifecycle.spec.ts
+++ tests/end-to-end/article-lifecycle.spec.ts
@@ -27,7 +27,7 @@ test.describe('Create, verify and delete article', () => {
   });
 
   test('Create new article @GAD_R04_01', async () => {
-    // Arrange test1
+    // Arrange
     articleData = randomNewArticle();
 
     //Act
