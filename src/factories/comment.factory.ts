47c0be0941240386e2009476b1b1e851fd242132
g0ph33r <pmpudysz@gmail.com>
2024-02-25 16:30:25 +0100

    wip: comment lifecycle, model and factory

---
+++ src/factories/comment.factory.ts
@@ -0,0 +1,9 @@
+import { AddCommentModel } from '../models/comment.model';
+import { faker } from '@faker-js/faker';
+
+export function prepareRandomComment(bodySentences = 3): AddCommentModel {
+  const body = faker.lorem.sentences(bodySentences);
+  const newComment: AddCommentModel = { body: body };
+
+  return newComment;
+}
