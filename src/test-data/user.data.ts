e71756561ba30f6d33950ad64c1faaad24e5268b
g0ph33r <pmpudysz@gmail.com>
2024-03-26 19:36:32 +0100

    chore: using alias to improve imports

--- src/test-data/user.data.ts
+++ src/test-data/user.data.ts
@@ -1,5 +1,5 @@
 import { USER_NAME, USER_PASSWORD } from '../env.config';
-import { LoginUserModel } from '../models/user.model';
+import { LoginUserModel } from '@_src/models/user.model';
 
 export const testUser1: LoginUserModel = {
   userEmail: USER_NAME,
