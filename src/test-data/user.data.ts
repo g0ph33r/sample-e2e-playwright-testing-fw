da70421373407d301c90b2f6c16e3f8ef88b269c
g0ph33r <pmpudysz@gmail.com>
2024-03-26 20:09:21 +0100

    chore: add alias for config

--- src/test-data/user.data.ts
+++ src/test-data/user.data.ts
@@ -1,4 +1,4 @@
-import { USER_NAME, USER_PASSWORD } from '../../config/env.config';
+import { USER_NAME, USER_PASSWORD } from '@_config/env.config';
 import { LoginUserModel } from '@_src/models/user.model';
 
 export const testUser1: LoginUserModel = {
