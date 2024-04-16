4d92e2c6a43a05ad65a0ae9b2299494b0d8f9ba4
g0ph33r <pmpudysz@gmail.com>
2024-04-17 00:18:30 +0200

    chore: wip adding fixtures page

--- tests/smoke/menu.spec.ts
+++ tests/smoke/menu.spec.ts
@@ -1,17 +1,25 @@
 import { ArticlesPage } from '@_src/pages/articles.page';
 import { CommentsPage } from '@_src/pages/comments.page';
-import { expect, test } from '@playwright/test';
+import { test as baseTest, expect } from '@playwright/test';
+
+const test = baseTest.extend<{ articlesPage: ArticlesPage }>({
+  articlesPage: async ({ page }, use) => {
+    const articlesPage = new ArticlesPage(page);
+    await articlesPage.goto();
+    await use(articlesPage);
+  },
+});
 
 test.describe('Verify main menu buttons', () => {
   test('Comments button navigates to comments page @GAD-R01-03', async ({
-    page,
+    articlesPage,
   }) => {
     // Arrange
-    const articlesPage = new ArticlesPage(page);
+
     const expectedCommentsTitle = 'Comments';
 
     // Act
-    await articlesPage.goto();
+
     const commentsPage = await articlesPage.mainMenu.clickCommentsButton();
     const title = await commentsPage.getTitle();
     // Assert
