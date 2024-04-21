3daeb7b67ded353bb544efce31ac22e86564c99d
g0ph33r <pmpudysz@gmail.com>
2024-04-21 22:23:15 +0200

    chore: own fixtures smoke tests

--- tests/smoke/webpage.spec.ts
+++ tests/smoke/webpage.spec.ts
@@ -1,12 +1,8 @@
-import { ArticlesPage } from '@_src/pages/articles.page';
-import { CommentsPage } from '@_src/pages/comments.page';
-import { HomePage } from '@_src/pages/home.page';
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify service main pages', () => {
-  test('Verify home page title @GAD-R01-01', async ({ page }) => {
+  test('Verify home page title @GAD-R01-01', async ({ homePage }) => {
     //Arrange
-    const homePage = new HomePage(page);
     const expectedHomePageTitle = 'GAD';
 
     //Act
@@ -18,37 +14,27 @@ test.describe('Verify service main pages', () => {
     expect(title).toContain(expectedHomePageTitle);
   });
 
-  test('Verify articles page title @GAD-R01-02', async ({ page }) => {
+  test('Verify articles page title @GAD-R01-02', async ({ articlesPage }) => {
     //Arrange
-    const articles = new ArticlesPage(page);
-    const expectedArticlesTitle = /Articles/;
-
-    //Act
-    await articles.goto();
+    const expectedArticlesTitle = '🦎 GAD | Articles';
 
     //Assert
-    await expect(page).toHaveTitle(expectedArticlesTitle);
+    const title = await articlesPage.getTitle();
+    expect(title).toContain(expectedArticlesTitle);
   });
 
-  test('Verify comments page title @GAD-R01-02', async ({ page }) => {
+  test('Verify comments page title @GAD-R01-02', async ({ commentsPage }) => {
     //Arrange
-
-    const comments = new CommentsPage(page);
-    const expectedCommentsTitle = /Comments/;
-
-    //Act
-    await comments.goto();
+    const expectedCommentsTitle = '🦎 GAD | Comments';
 
     //Assert
-    await expect(page).toHaveTitle(expectedCommentsTitle);
+    const title = await commentsPage.getTitle();
+    expect(title).toContain(expectedCommentsTitle);
   });
 
   test('Home page title simple', async ({ page }) => {
     //Act
-    await page.goto('/articles.html');
-
-    const newLocal = /Articles/;
-    //Assert
-    await expect(page).toHaveTitle(newLocal);
+    await page.goto('');
+    await expect(page).toHaveTitle(/GAD/);
   });
 });
