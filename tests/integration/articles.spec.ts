df0d9a000894228ce0fca127ca6efde978b2c132
g0ph33r <pmpudysz@gmail.com>
2024-06-11 20:40:04 +0200

    test: api verification after article creation

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -1,3 +1,4 @@
+import { RESPONSE_TIMEOUT } from '@_pw-config';
 import { prepareRandomArticle } from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
 import { waitForResponse } from '@_src/utils/wait.util';
@@ -77,6 +78,31 @@ test.describe('Verify articles', () => {
       // Assert
       expect(response.status()).toBe(expectedStatusCode);
       await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
+    });
+
+    test('should return created article from API @GAD-R04-02 @GAD-R07-03 @logged', async ({
+      addArticleView,
+      page,
+    }) => {
+      // Arrange
+      const articleData = prepareRandomArticle();
+      const responsePromise = page.waitForResponse(
+        (response) => {
+          return (
+            response.url().includes('/api/articles') &&
+            response.request().method() == 'GET'
+          );
+        },
+        { timeout: RESPONSE_TIMEOUT },
+      );
+
+      // Act
+      const articlePage = await addArticleView.createArticle(articleData);
+      const response = await responsePromise;
+
+      // Assert
+      await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
+      expect(response.ok()).toBeTruthy();
     });
   });
 });
