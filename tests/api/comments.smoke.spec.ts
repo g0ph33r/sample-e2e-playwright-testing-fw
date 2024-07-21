4bbfb988c2ecf985d199053928d31ee8a72364e0
g0ph33r <pmpudysz@gmail.com>
2024-07-21 20:29:59 +0200

    test: api smoke comments test

---
+++ tests/api/comments.smoke.spec.ts
@@ -0,0 +1,92 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
+
+test.describe('Verify comments API endpoint @GAD-R08-01 @api', () => {
+  test.describe('verify each condition in separate test', () => {
+    test('GET comments returns status code 200', async ({ request }) => {
+      // Arrange
+      const expectedStatusCode = 200;
+      const commentsUrl = '/api/comments';
+
+      // Act
+      const response = await request.get(commentsUrl);
+
+      // Assert
+      expect(response.status()).toBe(expectedStatusCode);
+    });
+    test('GET comments should return at least one comment @predefined_data', async ({
+      request,
+    }) => {
+      // Arrange
+      const expectedMincommentCount = 1;
+      const commentsUrl = '/api/comments';
+
+      // Act
+      const response = await request.get(commentsUrl);
+      const responseJson = await response.json();
+
+      // Assert
+      expect(responseJson.length).toBeGreaterThanOrEqual(
+        expectedMincommentCount,
+      );
+    });
+
+    test('GET comments return comment object @predefined_data', async ({
+      request,
+    }) => {
+      // Arrange
+      const expectedRequiredFields = [
+        'id',
+        'user_id',
+        'title',
+        'body',
+        'date',
+        'image',
+      ];
+
+      const commentsUrl = '/api/comments';
+
+      // Act
+      const response = await request.get(commentsUrl);
+      const responseJson = await response.json();
+      const comment = responseJson[0];
+
+      // Assert
+      expectedRequiredFields.forEach((key) => {
+        expect.soft(comment, `Key ${key} found in object`).toHaveProperty(key);
+      });
+    });
+  });
+});
+
+test('GET comments should return an object with required fields @predefined_data', async ({
+  request,
+}) => {
+  // Arrange
+  const commentsUrl = '/api/comments';
+  const response = await request.get(commentsUrl);
+  await test.step('GET comments returns status code 200', async () => {
+    const expectedStatusCode = 200;
+    expect(response.status()).toBe(expectedStatusCode);
+  });
+  const responseJson = await response.json();
+  await test.step('GET comments should return at least one comment', async () => {
+    const expectedMincommentCount = 1;
+    expect(responseJson.length).toBeGreaterThanOrEqual(expectedMincommentCount);
+  });
+
+  const expectedRequiredFields = [
+    'id',
+    'user_id',
+    'title',
+    'body',
+    'date',
+    'image',
+  ];
+
+  const comment = responseJson[0];
+  expectedRequiredFields.forEach(async (key) => {
+    await test.step(`response object contains required field: ${key}`, async () => {
+      expect.soft(comment).toHaveProperty(key);
+    });
+  });
+});
