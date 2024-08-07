fb4b590aac2d4c4eefbb8748271475ff61d81bb2
g0ph33r <pmpudysz@gmail.com>
2024-08-07 21:49:00 +0200

    test: articles integration api

---
+++ tests/api/articles.integration.spec.ts
@@ -0,0 +1,32 @@
+import { prepareRandomArticle } from '@_src/factories/article.factory';
+import { expect, test } from '@_src/fixtures/merge.fixture';
+
+
+test.describe('Verify articles CRUD operations @api', () => {
+  test('should not create an article without a logged-in user', async ({
+    request,
+  }) => {
+    // Arrange
+    const expectedStatusCode = 401;
+    const articlesUrl = '/api/articles';
+
+
+    const randomArticleData = prepareRandomArticle();
+    const articleData = {
+      title: randomArticleData.title,
+      body: randomArticleData.body,
+      date: '2024-06-30T15:44:31Z',
+      image: '',
+    };
+
+
+    // Act
+    const response = await request.post(articlesUrl, {
+      data: articleData,
+    });
+
+
+    // Assert
+    expect(response.status()).toBe(expectedStatusCode);
+  });
+});
